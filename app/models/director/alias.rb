ff5da7af42dd60633d027967431791c9bb5fd3d1
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2017-10-13 15:00:55 -0700

    Update all paths when the records change
    When either record changes we want their associated path to update. We were originally telling the source path not to update for performance reasons, but of course, it needs to still update if the source record is changed, so just update it all the time.

--- app/models/director/alias.rb
+++ app/models/director/alias.rb
@@ -1,6 +1,7 @@
 # TODO: Add pattern validation to allow base app to configure path whitelist
 # TODO: Add ability to limit formats so assets, json, etc. can be ignored if desired
-# TEST: source_path is set if only a source record is provided
+# TEST: source_path is set if a source record is provided
+# TEST: target_path is set if a target record is provided
 # TEST: can't create cycle
 
 module Director
@@ -15,8 +16,8 @@ module Director
     scope :with_source_path, -> { where.not(source_path: nil) }
     scope :with_target_path, -> { where.not(target_path: nil) }
 
-    before_save :set_source_path, unless: :source_path?
-
+    before_save :set_source_path, if: :source_changed?
+    before_save :set_target_path, if: :target_changed?
 
     def handler_class
       handler_name = "Director::Handler::#{handler.classify}"
@@ -37,6 +38,18 @@ module Director
 
     def set_source_path
       self.source_path = source.generate_canonical_path if source
+    end
+
+    def set_target_path
+      self.target_path = target.generate_canonical_path if target
+    end
+
+    def source_changed?
+      source_id_changed? || source_type_changed?
+    end
+
+    def target_changed?
+      target_id_changed? || target_type_changed?
     end
   end
 
